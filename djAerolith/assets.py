2177602d786c1f71eeb9cf2ee5d0080b76ee8566
Cesar Del Solar <delsolar@gmail.com>
2011-07-12 23:24:47 -0700

    fix broken icons/missing images jquery ui

--- djAerolith/assets.py
+++ djAerolith/assets.py
@@ -2,11 +2,16 @@ from django_assets import Bundle, register
 
 baseCss = Bundle('css/aerolith/aerolithStyleSheet.css',
                 'css/blog/blogStyles.css',
-                'css/redmond/jquery-ui-1.8.14.custom.css',
                 filters='cssmin',
                 output='css/aerolith/packedBaseSS.css')        
 
+jqueryCss = Bundle('css/redmond/jquery-ui-1.8.14.custom.css',
+                    filters='cssmin',
+                    output='css/redmond/packedJQUI.css')
+
+
 register('base_css', baseCss)
+register('jquery_css', jqueryCss)
 
 register('js_jquery', 'js/aerolith/jquery-1.6.1.min.js', 
-                            'js/aerolith/jquery-ui-1.8.14.custom.min.js')
+                        'js/aerolith/jquery-ui-1.8.14.custom.min.js')
