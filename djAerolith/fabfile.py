227b287483aa02b4b66f84b360c304c980c3116e
Cesar Del Solar <delsolar@gmail.com>
2011-10-04 01:05:22 -0700

    moving to new server.. ugh

--- djAerolith/fabfile.py
+++ djAerolith/fabfile.py
@@ -1,23 +1,24 @@
-from fabric.api import env, run,roles,cd, settings
+from fabric.api import env, run,roles,cd, settings, prefix
 import os
 
 env.key_filename= os.getenv("HOME") + "/Dropbox/aws/cesarkey.pem"
 env.roledefs = {
-    'prod': ['ec2-user@aerolith.org']
+    'prod': ['ubuntu@ec2-50-18-16-103.us-west-1.compute.amazonaws.com']
 }
 
 @roles('prod')
-def deploy_prod():
+def deploy_prod():    
     with settings(warn_only=True):
-        run("kill `cat /home/ec2-user/webolith/gunicorn.pid`")  # kill the gunicorn process
+        run("kill `cat /home/ubuntu/webolith/gunicorn.pid`")  # kill the gunicorn process    
     with cd("webolith"):
         run("git pull")
         with cd("djAerolith"):
             with settings(warn_only=True):
                 run("mkdir logs")
-            run("python manage.py collectstatic --noinput")  # collect static files!
-            run("python manage.py migrate") # execute any needed migrations
-            run("python manage.py run_gunicorn --config ../gunicornConf.py --daemon")
+            with prefix("workon aeroenv"):
+                run("python manage.py collectstatic --noinput")  # collect static files!
+                run("python manage.py migrate") # execute any needed migrations
+                run("python manage.py run_gunicorn --config ../gunicornConf.py --daemon")
 
 @roles('prod')
 def test_prod():
--- scripts/dailyCleanup.sh
+++ scripts/dailyCleanup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-cd /home/ec2-user/webolith/djAerolith
+workon aeroenv
+cd /home/ubuntu/webolith/djAerolith
 python manage.py deleteUnregisteredUsers delete
 python manage.py cleanTablegame 2
 python manage.py backup -e base_word -e base_alphagram -c --email delsolar@gmail.com
