257d1e4dcdea5cf5dc74436fb98f575ff886edc3
Cesar Del Solar <delsolar@gmail.com>
2012-03-12 01:18:38 -0700

    gen challenge stats beginnings

--- djAerolith/wordwalls/management/commands/genChallengeStats.py
+++ djAerolith/wordwalls/management/commands/genChallengeStats.py
@@ -1,20 +1,26 @@
-# cleans up DailyChallenges that are older than specified number of days in the past
-
+# generate challenge stats
 from django.core.management.base import BaseCommand, CommandError
 from wordwalls.models import (DailyChallenge,
                                 DailyChallengeLeaderboard,
                                 DailyChallengeLeaderboardEntry)
 
-from datetime import datetime, timedelta
+from django.conf import settings
+from datetime import datetime, timedelta, date
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        days = int(args[0])
-        delDate = datetime.now() - timedelta(days=days)
-        dcs = DailyChallenge.objects.filter(date__lt=delDate)
-        numObjs = len(dcs)
-        print "Found", numObjs, "objects to delete"
-        if numObjs > 0:
-            for dc in dcs:
-                dc.delete()
-            print "Deleted!"
+        frombeginning = False
+        if len(args) == 1:
+            if args[0] == 'frombeginning':
+                frombeginning = True
+        yesterday = date.today() - timedelta(days=1)
+        r = redis.Redis(host=settings.REDIS_HOST, port=settings.REDIS_PORT, db=1)
+        if frombeginning:
+            dcs = DailyChallenge.objects.all()
+        else:
+            dcs = DailyChallenge.objects.filter(date=)
+        for dc in dcs:
+            numObjs = len(dcs)
+            if numObjs > 0:
+                for dc in dcs:
+                    dc.delete()
