bb22bb6abb5ccff13a2e9ad19b0c554f5414f5f4
Cesar Del Solar <delsolar@gmail.com>
2012-07-20 23:20:08 -0700

    remove wordgrids from urls.py

--- djAerolith/urls.py
+++ djAerolith/urls.py
@@ -1,16 +1,16 @@
 # Aerolith 2.0: A web-based word game website
 # Copyright (C) 2011 Cesar Del Solar
-# 
+#
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -53,7 +53,6 @@ urlpatterns = patterns('',
     (r'^wordwalls/', include('wordwalls.urls')),
     (r'^flashcards/', include('whitleyCards.urls')),
     (r'^quackleInterface/', include('quackleInterface.urls')),
-    (r'^wordgrids/', include('wordgrids.urls')),
 
     (r'^mcc/', direct_to_template, {'template': 'misctools/menstrulator.html'})
 )
