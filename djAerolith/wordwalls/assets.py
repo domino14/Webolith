c91e2d9fe297b353ef1a9805bab1bd5964bafed6
Cesar Del Solar <delsolar@gmail.com>
2013-03-30 20:01:35 -0700

    Remove stars css

--- djAerolith/wordwalls/assets.py
+++ djAerolith/wordwalls/assets.py
@@ -44,13 +44,13 @@ tableCreateCss = Bundle('css/wordwalls/wordwallsCreateTable.css',
                         output='css/wordwalls/packedWWCT.css')
 
 fileUploaderCss = Bundle('css/aerolith/fileuploader.css',
-                        filters='cssmin',
-                        output='css/aerolith/packedFU.css')
+                         filters='cssmin',
+                         output='css/aerolith/packedFU.css')
 
 tableCss = Bundle('css/wordwalls/wordwallsTableSS.css',
-                    filters='cssmin',
-                    output='css/wordwalls/packedWWT.css')
+                  filters='cssmin',
+                  output='css/wordwalls/packedWWT.css')
 
 register('css_table_create', tableCreateCss)
-register('css_table','stars/jquery.ui.stars.min.css', tableCss)
+register('css_table', tableCss)
 register('css_fileuploader', fileUploaderCss)
