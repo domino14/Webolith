6557706809fe03e30ecc54300c2d2f9c58aef1e3
Cesar Del Solar <delsolar@gmail.com>
2013-05-30 11:30:04 -0700

    Fix bug where missing keys on server caused 500.

--- djAerolith/wordwalls/static/js/wordwalls/models/Configure.js
+++ djAerolith/wordwalls/static/js/wordwalls/models/Configure.js
@@ -1,8 +1,9 @@
 /* global define*/
 define([
   'backbone',
-  'jquery'
-], function(Backbone, $) {
+  'jquery',
+  'underscore'
+], function(Backbone, $, _) {
   var Configure;
   Configure = Backbone.Model.extend({
     defaults: function() {
@@ -16,19 +17,39 @@ define([
         showBorders: false
       };
     },
+    /**
+     * Maps server keys to a front-end model key.
+     * @param  {string} topLevelKey    Top level is 'tc' or 'bc'.
+     * @param  {string} secondLevelKey A sub-key like 'on' (for 'tc')
+     * @return {string}                The name of the Backbone model attribute.
+     */
+    mapServerKeys: function(topLevelKey, secondLevelKey) {
+      if (topLevelKey === 'tc') {
+        return {
+          'on': 'tilesOn',
+          'selection': 'tileSelection',
+          'font': 'font',
+          'bold': 'bold'
+        }[secondLevelKey];
+      } else if (topLevelKey === 'bc') {
+        return {
+          'showTable': 'showTable',
+          'showCanvas': 'showCanvas',
+          'showBorders': 'showBorders'
+        }[secondLevelKey];
+      }
+    },
     setConfig: function(params) {
-      var styleObj;
+      var styleObj, setObj;
       styleObj = $.parseJSON(params);
+      setObj = {};
       if (styleObj) {
-        this.set({
-          tilesOn: styleObj.tc.on,
-          tileSelection: styleObj.tc.selection,
-          font: styleObj.tc.font,
-          bold: styleObj.tc.bold,
-          showTable: styleObj.bc.showTable,
-          showCanvas: styleObj.bc.showCanvas,
-          showBorders: styleObj.bc.showBorders
-        });
+        _.each(styleObj, function(dict, key) {
+          _.each(dict, function(value, secondKey) {
+            setObj[this.mapServerKeys(key, secondKey)] = value;
+          }, this);
+        }, this);
+        this.set(setObj);
       }
     },
     url: '/wordwalls/api/configure/'
