e9792a4b6a7dc3e2d797d73f1f2b45997c3c864a
Cesar Del Solar <delsolar@gmail.com>
2013-08-23 00:41:36 -0700

    Script to export challenge data to CSV for viz :D

---
+++ djAerolith/wordwalls/management/commands/export_challenge_data.py
@@ -0,0 +1,45 @@
+from django.core.management.base import BaseCommand
+import json
+import csv
+from django.db import connection
+
+FILENAME = '/tmp/challenges_raw.csv'
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        """
+            Exports all challenge data to csv.
+        """
+
+        # Use a raw SQL query as it is about 10000 times faster or something
+        # like that.
+        query = """
+            select date, username, name, maxScore, score, timeRemaining,
+                lexiconName, additionalData
+                from wordwalls_dailychallengeleaderboardentry e
+            inner join auth_user u on
+                e.user_id = u.id
+            inner join wordwalls_dailychallengeleaderboard b on
+                e.board_id = b.id
+            inner join wordwalls_dailychallenge dc on
+                dc.id = b.challenge_id
+            inner join base_lexicon lex on
+                dc.lexicon_id = lex.id
+            inner join wordwalls_dailychallengename nm on
+                nm.id = dc.name_id order by date
+        """
+        cursor = connection.cursor()
+        cursor.execute(query)
+        with open(FILENAME, 'wb') as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerow(['Date', 'Username', 'Challenge Name', 'Max Score',
+                             'Score', 'Time Remaining', 'Lexicon', 'Medal'])
+            row = cursor.fetchone()
+            while row is not None:
+                try:
+                    medal = json.loads(row[-1]).get('medal')
+                except (TypeError, ValueError):
+                    medal = ''
+                writer.writerow(list(row[0:-1]) + [medal])
+                row = cursor.fetchone()
