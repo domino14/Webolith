d97b16d0d7c7cec23b7ded0807a60a25df0f869e
Cesar Del Solar <delsolar@gmail.com>
2013-11-05 00:10:02 -0800

    don't use so much memory creating the redis word db

--- djAerolith/base/management/commands/loadWordsIntoRedis.py
+++ djAerolith/base/management/commands/loadWordsIntoRedis.py
@@ -6,7 +6,7 @@ import json
 
 # Only use the following lexica:
 INCLUDE_LEX = [4, 6]        # 4 - OWL2, 6 - CSW12
-
+FETCH_MANY_SIZE = 1000
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
@@ -19,19 +19,21 @@ class Command(BaseCommand):
         cursor.execute(
             'SELECT alphagram, lexicon_id, probability_pk FROM '
             'base_alphagram WHERE lexicon_id IN %s' % str(tuple(INCLUDE_LEX)))
-        print 'Got all alphagrams, feeding into Redis...'
-        rows = cursor.fetchall()
+
+        rows = cursor.fetchmany(FETCH_MANY_SIZE)
         pipe = r.pipeline(transaction=False)
         rowCounter = 0
-        for row in rows:
-            key = '%s:%s' % (row[0], row[1])
-            value = row[2]
-            pipe.set(key, value)
-            rowCounter += 1
-            if rowCounter % 10000 == 0:
-                print ('%s .' % rowCounter),
-                pipe.execute()
-                pipe = r.pipeline()
+        while rows:
+            for row in rows:
+                key = '%s:%s' % (row[0], row[1])
+                value = row[2]
+                pipe.set(key, value)
+                rowCounter += 1
+                if rowCounter % 10000 == 0:
+                    print ('%s .' % rowCounter),
+                    pipe.execute()
+                    pipe = r.pipeline()
+            rows = cursor.fetchmany(FETCH_MANY_SIZE)
         print
         pipe.execute()
 
@@ -44,34 +46,35 @@ class Command(BaseCommand):
             'base_alphagram ON '
             'base_word.alphagram_id = base_alphagram.probability_pk WHERE '
             'base_word.lexicon_id in %s' % str(tuple(INCLUDE_LEX)))
-        print 'Got all words, feeding into redis...'
-        # Temporarily consume a bunch of memory?
-        rows = cursor.fetchall()
+        rows = cursor.fetchmany(FETCH_MANY_SIZE)
         r = redis.Redis(host=settings.REDIS_HOST, port=settings.REDIS_PORT,
                         db=settings.REDIS_ALPHAGRAM_SOLUTIONS_DB)
         pipe = r.pipeline()
         rowCounter = 0
         seen_alphas = set()
-        for row in rows:
-            obj = {
-                'word': row[0],
-                'symbols': row[2],
-                'def': row[3],
-                'f_hooks': row[4],
-                'b_hooks': row[5]
-            }
-            key = '%s' % row[1]  # Index by the alphagram id
-            if key not in seen_alphas:
-                pipe.delete(key)  # Delete the list and start over.
-                pipe.rpush(key, json.dumps({
-                    'question': row[6],
-                    'probability': row[7]
-                }))
-            seen_alphas.add(key)
-            pipe.rpush(key, json.dumps(obj))
-            rowCounter += 1
-            if rowCounter % 10000 == 0:
-                print ('%s .' % rowCounter),
-                pipe.execute()
-                pipe = r.pipeline()
+        while rows:
+            for row in rows:
+                obj = {
+                    'word': row[0],
+                    'symbols': row[2],
+                    'def': row[3],
+                    'f_hooks': row[4],
+                    'b_hooks': row[5]
+                }
+                key = '%s' % row[1]  # Index by the alphagram id
+                if key not in seen_alphas:
+                    pipe.delete(key)  # Delete the list and start over.
+                    pipe.rpush(key, json.dumps({
+                        'question': row[6],
+                        'probability': row[7]
+                    }))
+                seen_alphas.add(key)
+                pipe.rpush(key, json.dumps(obj))
+                rowCounter += 1
+                if rowCounter % 10000 == 0:
+                    print ('%s .' % rowCounter),
+                    pipe.execute()
+                    pipe = r.pipeline()
+            rows = cursor.fetchmany(FETCH_MANY_SIZE)
+        print
         pipe.execute()
