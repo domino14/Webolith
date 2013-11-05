3ba550b387624343a9172e692bc71d071ac7247c
Cesar Del Solar <delsolar@gmail.com>
2013-11-04 23:25:14 -0800

    fix mimetype stuff and add gzip to nginx

--- djAerolith/lib/response.py
+++ djAerolith/lib/response.py
@@ -5,7 +5,6 @@ from django.http import HttpResponse
 # returns an HttpResponse with a json-dump of obj
 def response(obj, status=200):
     resp = HttpResponse(json.dumps(obj, ensure_ascii=False),
-                        mimetype="application/javascript",
+                        content_type="application/javascript; charset=utf-8",
                         status=status)
-    resp['Content-Type'] = 'text/plain; charset=utf-8'
     return resp
