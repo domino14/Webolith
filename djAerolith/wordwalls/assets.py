01ce18eb21e7f4c6e029603cc38e777c5da899c2
Cesar Del Solar <delsolar@gmail.com>
2014-02-04 00:16:16 -0800

    get rid of some old files and clean up a little.

--- djAerolith/wordwalls/assets.py
+++ djAerolith/wordwalls/assets.py
@@ -1,17 +1,7 @@
 from django_assets import Bundle, register
 
-tableCreateCss = Bundle('css/wordwalls/wordwallsCreateTable.css',
-                        filters='cssmin',
-                        output='css/wordwalls/packedWWCT.css')
-
-fileUploaderCss = Bundle('css/aerolith/fileuploader.css',
-                         filters='cssmin',
-                         output='css/aerolith/packedFU.css')
-
 tableCss = Bundle('css/wordwalls/wordwallsTableSS.css',
                   filters='cssmin',
                   output='css/wordwalls/packedWWT.css')
 
-register('css_table_create', tableCreateCss)
 register('css_table', tableCss)
-register('css_fileuploader', fileUploaderCss)
