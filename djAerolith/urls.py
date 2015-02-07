1d1bc7061354e75a979c2f594567e3d6ee3a3cfc
Cesar Del Solar <delsolar@gmail.com>
2015-02-07 09:50:15 -0800

    fix django-registration URLs, password reset link.

--- djAerolith/urls.py
+++ djAerolith/urls.py
@@ -22,6 +22,7 @@ from django.contrib import admin
 #from registration.forms import RegistrationFormUniqueEmail
 from registration_app.forms import RecaptchaRegistrationForm
 from registration.backends.simple.views import RegistrationView
+from django.contrib.auth import views as auth_views
 
 import gargoyle
 
@@ -49,6 +50,27 @@ urlpatterns = patterns('',
     (r'^accounts/profile/', include('accounts.urls')),
     (r'^accounts/register/$', AerolithRegistrationView.as_view(
         form_class=RecaptchaRegistrationForm)),
+
+    #override the default registration urls
+    url(r'^accounts/password/change/$',
+        auth_views.password_change,
+        name='password_change'),
+    url(r'^accounts/password/change/done/$',
+        auth_views.password_change_done,
+        name='password_change_done'),
+    url(r'^accounts/password/reset/$',
+        auth_views.password_reset,
+        name='password_reset'),
+    url(r'^accounts/password/reset/done/$',
+        auth_views.password_reset_done,
+        name='password_reset_done'),
+    url(r'^accounts/password/reset/complete/$',
+        auth_views.password_reset_complete,
+        name='password_reset_complete'),
+    url(r'^accounts/password/reset/confirm/(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$',
+        auth_views.password_reset_confirm,
+        name='password_reset_confirm'),
+
     (r'^accounts/', include('registration.backends.simple.urls')),
     (r'^supporter/', 'views.supporter'),
     (r'^wordwalls/', include('wordwalls.urls')),
