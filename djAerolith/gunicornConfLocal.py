0e3887fc23b891c938a3cf0314fcea0eb29ff879
Cesar Del Solar <delsolar@gmail.com>
2016-02-19 00:45:13 -0800

    Oops.

--- djAerolith/gunicornConfLocal.py
+++
@@ -1,8 +0,0 @@
-import multiprocessing
-
-bind = "0.0.0.0:8000"
-workers = multiprocessing.cpu_count() * 2 + 1
-pidfile = "gunicorn.pid"
-logfile = "gunicorn.log"
-worker_class = "sync"
-timeout = 30
