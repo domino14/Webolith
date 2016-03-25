48156618b19cf9fb3a21b366834372e96251366d
Cesar Del Solar <delsolar@gmail.com>
2016-03-24 22:09:17 -0700

    A maintenance mode. Migrations in next commit.

---
+++ djAerolith/processors/maintenance.py
@@ -0,0 +1,15 @@
+import json
+
+from base.models import Maintenance
+
+
+def maintenance(request):
+    m = Maintenance.objects.filter(show=True)
+    if not m.count():
+        return {
+            'show_maintenance': False
+        }
+    return {
+        'show_maintenance': m[0].show,
+        'info': json.loads(m[0].info)
+    }
