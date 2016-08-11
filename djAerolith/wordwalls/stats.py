eff126a003290e4eecaeebf9f2058f043f7b0df3
Emily Dowgialo <emilydowgialo@gmail.com>
2016-08-10 19:00:02 -0700

    medals leaderboard backend

--- djAerolith/wordwalls/stats.py
+++ djAerolith/wordwalls/stats.py
@@ -5,6 +5,7 @@ from wordwalls.models import (DailyChallengeName,
                               DailyChallenge,
                               DailyChallengeLeaderboard,
                               DailyChallengeLeaderboardEntry)
+from accounts.models import AerolithProfile
 from base.models import Lexicon
 import json
 from lib.response import response
@@ -13,6 +14,7 @@ from lib.response import response
 # If not logged in, will ask user to log in and forward back to the main url
 @login_required
 def main(request):
+
     return render(request, 'wordwalls/stats.html')
 
 
@@ -61,3 +63,65 @@ def get_stats(request, lexicon, type_of_challenge_id):
         info_we_want.append(entry_info)
 
     return response(info_we_want)
+
+
+@login_required
+def leaderboard(request):
+
+    return render(request, 'wordwalls/leaderboard.html')
+
+
+@login_required
+def get_medals(request):
+
+    profiles = AerolithProfile.objects.all()
+
+    # {user, dict of medals}
+    all_medals = []
+
+    # {user: sum of their medals}
+    medal_totals = {}
+
+    ########################################################
+
+    def add_medals(user):
+        """ Sum user's medals """
+
+        # Access the dict of medals
+        medals_dict = user[1]
+        medals = medals_dict["medals"]
+
+        # Convert medal numbers to integers
+        for key in medals:
+            medals[key] = int(medals[key])
+
+        # Sum medal values
+        medals_total = sum(medals.values())
+
+        return medals_total
+
+    ########################################################
+
+    # Weed out users with no medals
+    for profile in profiles:
+
+        medals = None
+
+        user = profile.user.username
+        if profile.wordwallsMedals is not None and profile.wordwallsMedals != "":
+            medals = json.loads(profile.wordwallsMedals)
+            if len(medals) < 1:
+                continue
+        else:
+            continue
+
+        user_medals = [user, medals]
+        all_medals.append(user_medals)
+
+    # Sum a user's medals, then add the user: total medals key value pair
+    # to medal_totals
+    for user in all_medals:
+        total_medals = add_medals(user)
+        medal_totals[user[0]] = total_medals
+
+    return response(medal_totals)
