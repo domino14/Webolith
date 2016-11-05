2825b9985e73676e53b88ff2be4800e82505f0b3
Cesar Del Solar <delsolar@gmail.com>
2016-11-04 23:44:06 -0700

    Trying to get r.js to build.

--- djAerolith/js_build/table_wordwalls.js
+++ djAerolith/js_build/table_wordwalls.js
@@ -1,6 +1,6 @@
 ({
   baseUrl: "../wordwalls/static/js/wordwalls",
-  mainConfigFile: '../wordwalls/static/js/wordwalls/main.js',
-  name: "main",
-  out: "../static/build/table-main-built.js"
+  mainConfigFile: '../wordwalls/static/js/wordwalls/mainreact.js',
+  name: "mainreact",
+  out: "../static/build/table-main-react-built.js"
 })
