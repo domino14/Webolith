7b0f0e752a2730b47d5cad20db2b61292b987f78
Cesar Del Solar <delsolar@gmail.com>
2016-11-22 00:14:04 -0800

    Are you sure? button for give up.

--- djAerolith/wordwalls/static/js/wordwalls/reactapp/topbar/start_button.jsx
+++ djAerolith/wordwalls/static/js/wordwalls/reactapp/topbar/start_button.jsx
@@ -1,40 +1,73 @@
 import React from 'react';
-import $ from 'jquery';
 
-const DISABLE_BUTTON_TIMEOUT = 1000;
+const BUTTON_STATE_START = 1;
+const BUTTON_STATE_GIVEUP = 2;
+const BUTTON_STATE_YOUSURE = 3;
+
+const YOU_SURE_TIMEOUT = 3000;
 
 class StartButton extends React.Component {
   constructor() {
     super();
     this.handleButtonClick = this.handleButtonClick.bind(this);
+    this.state = {
+      buttonState: BUTTON_STATE_START,
+    };
   }
 
   handleButtonClick() {
     if (this.props.gameGoing) {
-      this.props.handleGiveup();
+      if (this.state.buttonState === BUTTON_STATE_GIVEUP) {
+        this.setState({
+          buttonState: BUTTON_STATE_YOUSURE,
+        });
+
+        window.setTimeout(() => {
+          if (this.props.gameGoing) {
+            this.setState({
+              buttonState: BUTTON_STATE_GIVEUP,
+            });
+          } else {
+            this.setState({
+              buttonState: BUTTON_STATE_START,
+            });
+          }
+        }, YOU_SURE_TIMEOUT);
+      } else if (this.state.buttonState === BUTTON_STATE_YOUSURE) {
+        this.props.handleGiveup();
+        window.clearTimeout(this.state.changeTimeout);
+        this.setState({
+          buttonState: BUTTON_STATE_START,
+          changeTimeout: null,
+        });
+      }
     } else {
       this.props.handleStart();
+      this.setState({
+        buttonState: BUTTON_STATE_GIVEUP,
+      });
     }
-    $(this.button).prop('disabled', true);
-    window.setTimeout(() => $(this.button).prop('disabled', false),
-      DISABLE_BUTTON_TIMEOUT);
   }
 
   render() {
     let buttonText;
     let buttonClass;
-    if (this.props.gameGoing) {
-      buttonText = 'Give Up';
-      buttonClass = 'btn btn-danger btn-sm';
-    } else {
+
+    // No matter what, if the game isn't going, display a Start button.
+    if (!this.props.gameGoing) {
       buttonText = 'Start';
       buttonClass = 'btn btn-primary btn-sm';
+    } else if (this.state.buttonState === BUTTON_STATE_GIVEUP) {
+      buttonText = 'Give Up';
+      buttonClass = 'btn btn-danger btn-sm';
+    } else if (this.state.buttonState === BUTTON_STATE_YOUSURE) {
+      buttonText = 'Are you sure?';
+      buttonClass = 'btn btn-danger btn-sm';
     }
 
     return (
       <button
         className={buttonClass}
-        ref={btn => (this.button = btn)}
         onClick={this.handleButtonClick}
         style={{
           marginTop: '-6px',
