084e2d903e245b7a163e308920b15d11f6ae0f9d
Cesar Del Solar <delsolar@gmail.com>
2017-01-04 09:49:05 -0800

    Use a table to show saved lists, instead of cards.

--- djAerolith/wordwalls/static/js/wordwalls/reactapp/newtable/saved_list_dialog.jsx
+++ djAerolith/wordwalls/static/js/wordwalls/reactapp/newtable/saved_list_dialog.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import $ from 'jquery';
 import Notifications from '../notifications';
 
-import ListCard from './list_card';
+import ListTable from './list_table';
 
 class SavedListDialog extends React.Component {
   static genOptions(listOptions) {
@@ -12,17 +12,7 @@ class SavedListDialog extends React.Component {
     }));
     return opts;
   }
-  /**
- *           <Select
-            colSize={9}
-            numItems={15}
-            label="My Saved Lists"
-            selectedValue={this.props.selectedList}
-            onChange={event => this.props.onSelectedListChange(event.target.value)}
-            options={SavedListDialog.genOptions(this.props.listOptions)}
-          />
- * @return {[type]} [description]
- */
+
   constructor() {
     super();
     this.continueList = this.continueList.bind(this);
@@ -38,7 +28,11 @@ class SavedListDialog extends React.Component {
   // For most of the following, confirm that the user actually wants to
   // do this action.
   continueList(listID) {
-    this.props.onListSubmit(listID, 'continue');
+    Notifications.confirm(
+      'Are you sure?',
+      `Are you sure you wish to continue this list? You will lose any
+      unsaved progress on your current lists.`,
+      () => this.props.onListSubmit(listID, 'continue'));
   }
 
   playFirstMissed(listID) {
@@ -64,36 +58,25 @@ class SavedListDialog extends React.Component {
   }
 
   render() {
-    const cards = this.props.listOptions.lists.map((option, idx) => (
-      <div className="col-sm-6 col-md-4" key={idx}>
-        <ListCard
-          listID={option.id}
-          listName={option.name}
-          lastSaved={option.lastSaved}
-          lastSavedDT={option.lastSavedDT}
-          progress={option.questionIndex > option.numCurAlphagrams ?
-            option.numCurAlphagrams : option.questionIndex
-            // A bug where questionIndex always seems to be a multiple
-            // of 50 even if the quiz is done.
-          }
-          totalCurQuestions={option.numCurAlphagrams}
-          goneThruOnce={option.goneThruOnce}
-          continueList={listID => () => this.continueList(listID)}
-          playFirstMissed={listID => () => this.playFirstMissed(listID)}
-          resetStartOver={listID => () => this.resetStartOver(listID)}
-          deleteList={listID => () => this.deleteList(listID)}
-        />
-      </div>
-    ));
-
     return (
       <div className="row">
-        <div className="col-sm-12">
+        <div className="col-sm-11">
+          <div className="row">
+            Please select a list from below. Lists that are highlighted
+            in <span className="bg-success">green</span> have already been
+            played through once.
+          </div>
           <div
             className="row"
-            style={{ maxHeight: 450, overflow: 'scroll' }}
+            style={{ maxHeight: 450, minHeight: 350, overflow: 'scroll' }}
           >
-            {cards}
+            <ListTable
+              lists={this.props.listOptions.lists}
+              continueList={listID => () => this.continueList(listID)}
+              playFirstMissed={listID => () => this.playFirstMissed(listID)}
+              resetStartOver={listID => () => this.resetStartOver(listID)}
+              deleteList={listID => () => this.deleteList(listID)}
+            />
           </div>
         </div>
       </div>
