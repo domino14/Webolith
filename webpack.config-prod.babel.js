f72a7a2649ee61371b9b8178338e08052345215d
Cesar Del Solar <delsolar@gmail.com>
2017-02-23 19:46:24 -0800

    Some tweaks to webpack and styling.

--- webpack.config-prod.babel.js
+++ webpack.config-prod.babel.js
@@ -1,56 +1,28 @@
 /* eslint-disable import/no-extraneous-dependencies */
 /* Use for production build. Minifies, etc. */
-const webpack = require('webpack');
+// use import here instead of require, as require adds a 'default' key.
+import webpackDev from './webpack.config.babel';
 
-export default {
-  output: {
-    filename: 'table-client-bundle.js',
-    publicPath: '/static/dist/',
-  },
-  devtool: 'source-map',
-  module: {
-    loaders: [
-      {
-        test: /\.jsx?$/,
-        loader: 'babel-loader',
-        exclude: [/node_modules/],
-      },
-      {
-        test: /\.(woff|woff2)$/,
-        loader: 'url-loader?limit=10000&mimetype=application/font-woff',
-      },
-      {
-        test: /\.ttf$/,
-        loader: 'file-loader',
-      },
-      {
-        test: /\.eot$/,
-        loader: 'file-loader',
-      },
-      {
-        test: /\.svg$/,
-        loader: 'file-loader',
-      },
+const webpack = require('webpack');
+const _ = require('underscore');
+
+const prodConfig = _.defaults(
+  {
+    plugins: [
+      new webpack.ProvidePlugin({
+        $: 'jQuery',
+        jQuery: 'jquery',
+      }),
+      new webpack.DefinePlugin({
+        'process.env': {
+          // For minifying React correctly.
+          NODE_ENV: JSON.stringify('production'),
+        },
+      }),
+      new webpack.optimize.DedupePlugin(),
+      new webpack.optimize.UglifyJsPlugin(),
+      new webpack.optimize.AggressiveMergingPlugin(),
     ],
-  },
-  resolve: {
-    extensions: ['', '.js', '.jsx'],
-  },
-  entry: ['bootstrap-webpack',
-    './djAerolith/wordwalls/static/js/wordwalls/reactapp/index'],
-  plugins: [
-    new webpack.ProvidePlugin({
-      $: 'jQuery',
-      jQuery: 'jquery',
-    }),
-    new webpack.DefinePlugin({
-      'process.env': {
-        // For minifying React correctly.
-        NODE_ENV: JSON.stringify('production'),
-      },
-    }),
-    new webpack.optimize.DedupePlugin(),
-    new webpack.optimize.UglifyJsPlugin(),
-    new webpack.optimize.AggressiveMergingPlugin(),
-  ],
-};
+  }, webpackDev);
+
+export default prodConfig;
