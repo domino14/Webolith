ad88af1e8aecc6bf476ae43282d66fb2d68b9482
Cesar Del Solar <delsolar@gmail.com>
2022-01-01 23:24:00 -0500

    use 3rd party enzyme adapter for test

--- djAerolith/wordwalls/static/js/wordwalls/test/setup_enzyme.js
+++ djAerolith/wordwalls/static/js/wordwalls/test/setup_enzyme.js
@@ -1,8 +1,7 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import Enzyme from 'enzyme';
-import EnzymeAdapter from 'enzyme-adapter-react-16';
-
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 // Setup enzyme's react adapter
-Enzyme.configure({ adapter: new EnzymeAdapter() });
+Enzyme.configure({ adapter: new Adapter() });
 // Mocking the Headers from fetch
 global.Headers = () => {};
