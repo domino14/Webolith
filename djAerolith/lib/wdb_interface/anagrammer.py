ef865af300e20b17170677ab4ed1b7c9064f0854
Cesar Del Solar <delsolar@gmail.com>
2022-01-08 00:55:42 -0500

    update twirp library and add new deleted words search condition

--- djAerolith/lib/wdb_interface/anagrammer.py
+++ djAerolith/lib/wdb_interface/anagrammer.py
@@ -2,25 +2,32 @@ import logging
 from typing import List
 
 from django.conf import settings
+from twirp.context import Context
+from twirp.exceptions import TwirpServerException
 
 from lib.wdb_interface.exceptions import WDBError
-from rpc.wordsearcher.searcher_pb2_twirp import (AnagrammerClient,
-                                                 TwirpException)
+from rpc.wordsearcher.searcher_twirp import AnagrammerClient
 import rpc.wordsearcher.searcher_pb2 as pb
+
 logger = logging.getLogger(__name__)
 
 
 def resp_to_alphagram_dicts(resp):
     alpha_dicts = []
     for q in resp.alphagrams:
-        to_append = {'q': q.alphagram, 'a': [w.word for w in q.words]}
+        to_append = {"q": q.alphagram, "a": [w.word for w in q.words]}
         alpha_dicts.append(to_append)
 
     return alpha_dicts
 
 
-def gen_blank_challenges(length: int, lexicon_name: str, num_2_blanks: int,
-                         num_questions: int, max_answers: int) -> List[dict]:
+def gen_blank_challenges(
+    length: int,
+    lexicon_name: str,
+    num_2_blanks: int,
+    num_questions: int,
+    max_answers: int,
+) -> List[dict]:
     """
     Generate a set of blank challenges with the given parameters.
 
@@ -31,18 +38,24 @@ def gen_blank_challenges(length: int, lexicon_name: str, num_2_blanks: int,
         num_questions=num_questions,
         max_solutions=max_answers,
         num_with_2_blanks=num_2_blanks,
-        word_length=length)
+        word_length=length,
+    )
 
     try:
-        response = client.blank_challenge_creator(sr)
-    except TwirpException as e:
+        response = client.BlankChallengeCreator(ctx=Context(), request=sr)
+    except TwirpServerException as e:
         raise WDBError(e)
     return resp_to_alphagram_dicts(response)
 
 
-def gen_build_challenge(min_length: int, max_length: int, lexicon_name: str,
-                        require_length_solution: bool, min_solutions: int,
-                        max_solutions: int):
+def gen_build_challenge(
+    min_length: int,
+    max_length: int,
+    lexicon_name: str,
+    require_length_solution: bool,
+    min_solutions: int,
+    max_solutions: int,
+):
     client = AnagrammerClient(settings.WORD_DB_SERVER_ADDRESS)
     sr = pb.BuildChallengeCreateRequest(
         lexicon=lexicon_name,
@@ -50,26 +63,23 @@ def gen_build_challenge(min_length: int, max_length: int, lexicon_name: str,
         max_solutions=max_solutions,
         min_length=min_length,
         max_length=max_length,
-        require_length_solution=require_length_solution
+        require_length_solution=require_length_solution,
     )
     try:
-        response = client.build_challenge_creator(sr)
-    except TwirpException as e:
+        response = client.BuildChallengeCreator(ctx=Context(), request=sr)
+    except TwirpServerException as e:
         raise WDBError(e)
     return resp_to_alphagram_dicts(response)
 
 
-def anagram_letters(lexicon_name: str, letters: str,
-                    mode=pb.AnagramRequest.Mode.EXACT):
+def anagram_letters(
+    lexicon_name: str, letters: str, mode=pb.AnagramRequest.Mode.EXACT
+):
     client = AnagrammerClient(settings.WORD_DB_SERVER_ADDRESS)
-    sr = pb.AnagramRequest(
-        lexicon=lexicon_name,
-        letters=letters,
-        mode=mode
-    )
+    sr = pb.AnagramRequest(lexicon=lexicon_name, letters=letters, mode=mode)
     try:
-        response = client.anagram(sr)
-    except TwirpException as e:
+        response = client.Anagram(ctx=Context(), request=sr)
+    except TwirpServerException as e:
         raise WDBError(e)
     words = [w.word for w in response.words]
     return words
--- djAerolith/lib/wdb_interface/wdb_helper.py
+++ djAerolith/lib/wdb_interface/wdb_helper.py
@@ -1,33 +1,38 @@
 from typing import List
 
 from django.conf import settings
+from twirp.context import Context
+from twirp.exceptions import TwirpServerException
 
 from base.models import Lexicon
 from lib.domain import Questions
 from lib.wdb_interface.exceptions import WDBError
 from lib.wdb_interface.word_searches import SearchDescription
-from rpc.wordsearcher.searcher_pb2_twirp import (
-    QuestionSearcherClient, TwirpException)
+from rpc.wordsearcher.searcher_twirp import QuestionSearcherClient
 import rpc.wordsearcher.searcher_pb2 as pb
 
 
-def questions_from_alphagrams(lexicon: Lexicon,
-                              alphas: List[str],
-                              expand: bool = False) -> Questions:
+def questions_from_alphagrams(
+    lexicon: Lexicon, alphas: List[str], expand: bool = False
+) -> Questions:
     """
     Given a list of alphagrams, get optionally fully populated questions.
 
     """
     # build search request
-    qs = word_search([
-        SearchDescription.lexicon(lexicon),
-        SearchDescription.alphagram_list(alphas),
-    ], expand)
+    qs = word_search(
+        [
+            SearchDescription.lexicon(lexicon),
+            SearchDescription.alphagram_list(alphas),
+        ],
+        expand,
+    )
     return qs
 
 
-def questions_from_alpha_dicts(lexicon: Lexicon,
-                               alphas: List[dict]) -> Questions:
+def questions_from_alpha_dicts(
+    lexicon: Lexicon, alphas: List[dict]
+) -> Questions:
     """
     This has to use the client.expand function.
     alphas looks like:
@@ -40,53 +45,61 @@ def questions_from_alpha_dicts(lexicon: Lexicon,
     pbas = []
     for alpha in alphas:
         words = []
-        for w in alpha['a']:
+        for w in alpha["a"]:
             words.append(pb.Word(word=w))
 
-        pba = pb.Alphagram(alphagram=alpha['q'])
+        pba = pb.Alphagram(alphagram=alpha["q"])
         pba.words.extend(words)
         pbas.append(pba)
 
     sr.alphagrams.extend(pbas)
     try:
-        response = client.expand(sr)
-    except TwirpException as e:
+        response = client.Expand(ctx=Context(), request=sr)
+    except TwirpServerException as e:
         raise WDBError(e)
     qs = Questions()
     qs.set_from_pb_alphagrams(response.alphagrams)
     return qs
 
 
-def questions_from_probability_range(lexicon: Lexicon,
-                                     pmin: int, pmax: int, wl: int,
-                                     expand: bool = False):
-    qs = word_search([
-        SearchDescription.lexicon(lexicon),
-        SearchDescription.length(wl, wl),
-        SearchDescription.probability_range(pmin, pmax),
-    ], expand)
+def questions_from_probability_range(
+    lexicon: Lexicon, pmin: int, pmax: int, wl: int, expand: bool = False
+):
+    qs = word_search(
+        [
+            SearchDescription.lexicon(lexicon),
+            SearchDescription.length(wl, wl),
+            SearchDescription.probability_range(pmin, pmax),
+        ],
+        expand,
+    )
     return qs
 
 
-def questions_from_probability_list(lexicon: Lexicon, plist: List[int],
-                                    wl: int, expand: bool = False):
-    qs = word_search([
-        SearchDescription.lexicon(lexicon),
-        SearchDescription.length(wl, wl),
-        SearchDescription.probability_list(plist),
-    ], expand)
+def questions_from_probability_list(
+    lexicon: Lexicon, plist: List[int], wl: int, expand: bool = False
+):
+    qs = word_search(
+        [
+            SearchDescription.lexicon(lexicon),
+            SearchDescription.length(wl, wl),
+            SearchDescription.probability_list(plist),
+        ],
+        expand,
+    )
     return qs
 
 
-def word_search(search_descriptions: List[pb.SearchRequest.SearchParam],
-                expand=False) -> Questions:
+def word_search(
+    search_descriptions: List[pb.SearchRequest.SearchParam], expand=False
+) -> Questions:
     client = QuestionSearcherClient(settings.WORD_DB_SERVER_ADDRESS)
     sr = pb.SearchRequest()
     sr.expand = expand
     sr.searchparams.extend(search_descriptions)
     try:
-        response = client.search(sr)
-    except TwirpException as e:
+        response = client.Search(ctx=Context(), request=sr)
+    except TwirpServerException as e:
         raise WDBError(e)
     qs = Questions()
     qs.set_from_pb_alphagrams(response.alphagrams)
