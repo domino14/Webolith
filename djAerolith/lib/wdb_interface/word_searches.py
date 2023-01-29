d5e0bb1b2e95d2b1e7958b142c352e87e729bf3c
César Del Solar <delsolar@gmail.com>
2023-01-28 21:10:42 -0500

    OSPS48

--- djAerolith/lib/wdb_interface/word_searches.py
+++ djAerolith/lib/wdb_interface/word_searches.py
@@ -229,8 +229,8 @@ def temporary_list_name(
                     desc = "CSW19"
                 elif lexicon_name == "FISE2":
                     desc = "FISE09"
-                elif lexicon_name == "OSPS46":
-                    desc = "OSPS44"
+                elif lexicon_name == "OSPS48":
+                    desc = "OSPS46"
             tokens.append(f"not in {desc}")
         elif sd.condition == pb.SearchRequest.Condition.MATCHING_ANAGRAM:
             tokens.append(f"matching {sd.stringvalue.value}")
