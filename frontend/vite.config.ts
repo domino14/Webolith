036f0e6f121161f93ea438acca7679ba1adf23eb
César Del Solar <delsolar@gmail.com>
2024-10-23 18:25:15 -0400

    use vite pwa plugin isntead of manifest json file

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,49 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        short_name: "WordVault",
+        name: "Aerolith WordVault",
+        icons: [
+          {
+            src: "pwa/manifest-icon-192.maskable.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "pwa/manifest-icon-192.maskable.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "maskable",
+          },
+          {
+            src: "pwa/manifest-icon-512.maskable.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "pwa/manifest-icon-512.maskable.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
+        start_url: "/wordvault/",
+        scope: "/wordvault/",
+        display: "fullscreen",
+        theme_color: "#000000",
+        background_color: "#ffffff",
+      },
+    }),
+  ],
   base: "/wordvault",
 });
