ebb980414571f008fb551078629f0204ae5ede50
César Del Solar <delsolar@gmail.com>
2025-08-29 00:56:07 -0400

    persistent db conns

--- djAerolith/djaerolith/settings.py
+++ djAerolith/djaerolith/settings.py
@@ -63,6 +63,7 @@ DATABASES = {
         "HOST": os.environ.get("PGSQL_HOST"),
         "PORT": os.environ.get("PGSQL_PORT", "5432"),
         "ATOMIC_REQUESTS": True,
+        "CONN_MAX_AGE": 600,
     }
 }
 
