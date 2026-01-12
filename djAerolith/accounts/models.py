c425896bd41c99daab6c30aea0ad87c13a32026e
César Del Solar <delsolar@gmail.com>
2026-01-11 23:38:06 -0500

    update to OSPS51 for profile

--- djAerolith/accounts/models.py
+++ djAerolith/accounts/models.py
@@ -32,7 +32,7 @@ def getLexicon(request=None):
     elif request.LANGUAGE_CODE == "es":
         return Lexicon.objects.get(lexiconName="FISE2")
     elif request.LANGUAGE_CODE == "pl":
-        return Lexicon.objects.get(lexiconName="OSPS50")
+        return Lexicon.objects.get(lexiconName="OSPS51")
     elif request.LANGUAGE_CODE == "de":
         return Lexicon.objects.get(lexiconName="RD29")
     elif request.LANGUAGE_CODE == "fr":
